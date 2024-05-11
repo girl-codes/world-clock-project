b0aa133cc131e7820b441d15c38e7d6400d38057
girl-codes <168541859+girl-codes@users.noreply.github.com>
2024-05-11 17:02:40 +0100

    Added JS to the project
    - Added JS to project so the date and time is live
    - Time is live and updated per second
    - Removed dummy content from HTML so that correct time / date is shown when page has loaded

--- javascript/interaction.js
+++ javascript/interaction.js
@@ -1,1 +1,24 @@
+function updateTime() {
+  let parisElement = document.querySelector("#paris");
+  let parisDateElement = parisElement.querySelector(".date");
+  let parisTimeElement = parisElement.querySelector(".time");
+  let parisTime = moment().tz("Europe/Paris");
 
+  parisDateElement.innerHTML = moment().format("Do MMMM YYYY");
+  parisTimeElement.innerHTML = parisTime.format(
+    "HH:mm:ss [<small>]A[</small>]"
+  );
+
+  let cagliariElement = document.querySelector("#cagliari");
+  let cagliariDateElement = cagliariElement.querySelector(".date");
+  let cagliariTimeElement = cagliariElement.querySelector(".time");
+  let cagliariTime = moment().tz("CET");
+
+  cagliariDateElement.innerHTML = moment().format("Do MMMM YYYY");
+  cagliariTimeElement.innerHTML = cagliariTime.format(
+    "HH:mm:ss [<small>]A[</small>]"
+  );
+}
+
+updateTime();
+setInterval(updateTime, 1000);
