5cddb3d635bb5ca6930648128681a775c51ad221
girl-codes <168541859+girl-codes@users.noreply.github.com>
2024-05-11 18:26:19 +0100

    Changed one of main cities  & Added some CSS
    - Changed one of main cities from Paris to London
    - Added CSS to date

--- javascript/interaction.js
+++ javascript/interaction.js
@@ -1,11 +1,11 @@
 function updateTime() {
-  let parisElement = document.querySelector("#paris");
-  let parisDateElement = parisElement.querySelector(".date");
-  let parisTimeElement = parisElement.querySelector(".time");
-  let parisTime = moment().tz("Europe/Paris");
+  let londonElement = document.querySelector("#london");
+  let londonDateElement = londonElement.querySelector(".date");
+  let londonTimeElement = londonElement.querySelector(".time");
+  let londonTime = moment().tz("Europe/London");
 
-  parisDateElement.innerHTML = moment().format("Do MMMM YYYY");
-  parisTimeElement.innerHTML = parisTime.format(
+  londonDateElement.innerHTML = moment().format("Do MMMM YYYY");
+  londonTimeElement.innerHTML = londonTime.format(
     "HH:mm:ss [<small>]A[</small>]"
   );
 
